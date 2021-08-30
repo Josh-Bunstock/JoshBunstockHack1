9761d0e8e782384f9a3de5ac33dd2ba98daf665d
Josh Bunstock <jbunstock4@huskers.unl.edu>
2021-08-30 22:42:41 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Josh Bunstock
+ *         jbunstock@unl.edu
+ * Date: 2021/08/26
+ *
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Josh Bunstock!\n");
+  printf("Computer Engineering or Electrical Engineering\n");
+
+  return 0;
+}
